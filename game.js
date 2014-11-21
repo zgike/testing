70b00be1fba584f4f50781932a3c5f6257fd8c36
zgike <theskullee@gmail.com>
2014-11-22 00:15:55 +1300

    i dont know what im programming

--- game.js
+++ game.js
@@ -1,21 +1,45 @@
 const CHUNK_SIZE = 16;
 
 var mapWorker = new Worker("mapWorker.js");
+var position = {x:0,y:0}
 
-mapWorker.postMessage({x:CHUNK_SIZE * 3,y:CHUNK_SIZE * 3});
+function color(n) {
+	return "rgb(" + n + "," + n + "," + n + ")";
+}
 
 mapWorker.onmessage = function (e) {
 	var result = "\n";
 	for (var i = 0; i < CHUNK_SIZE; i++) {
-		result += e.data[i * CHUNK_SIZE].toString(10);
-		for (var j = 1; j < CHUNK_SIZE; j++) {
-			var value = e.data[i * CHUNK_SIZE + j].toString(10);
-			while (value.length < 4) {
-				value = " " + value;			
+		for (var j = 0; j < CHUNK_SIZE; j++) {
+			var value = e.data[i * CHUNK_SIZE + j];
+			var str = value.toString(10);
+			while (str.length < 3) {
+				str = "0" + str;			
 			}
-			result += value;
+			result += "<span style=\"color:" + color(value) + ";\"> " + str + "</span>";
 		}
-		result += "\n";
+		result += "<br>";
 	}
-	console.log(result);
+	document.body.innerHTML = result;
 };
+
+document.addEventListener("keydown", function (e) {
+	if (e.keyCode >= 37 && e.keyCode <= 40) {
+		e.preventDefault();
+		switch (e.keyCode) {
+			case 37:
+				position.x--;
+				break;
+			case 38:
+				position.y++;
+				break;
+			case 39:
+				position.x++;
+				break;
+			case 40:
+				position.y--;
+				break;
+		}
+		mapWorker.postMessage(position);
+	}
+}, false);
--- mapWorker.js
+++ mapWorker.js
@@ -6,7 +6,9 @@ self.onmessage = function (e) {
 	var chunk = new Uint8Array(CHUNK_SIZE * CHUNK_SIZE);
 	for (var i = 0; i < CHUNK_SIZE; i++) {
 		for (var j = 0; j < CHUNK_SIZE; j++) {
-			chunk[i * CHUNK_SIZE + j] = Math.floor(Math.sin(i/10) * Math.sin(j/10) * 10);
+			chunk[i * CHUNK_SIZE + j] =
+				200 * Math.floor(Math.sin((i -  e.data.y) / 2)
+				+ Math.sin((j + e.data.x) / 2));
 		}
 	}
 	self.postMessage(chunk);
