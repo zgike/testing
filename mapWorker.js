ead76b401b4ba7c439c46c0c47563695f4d7fef4
zgike <theskullee@gmail.com>
2014-11-22 01:30:35 +1300

    colors and shit

--- mapWorker.js
+++ mapWorker.js
@@ -4,19 +4,26 @@ function color(n) {
 	return "rgb(" + (127 - n) + "," + n + "," + (255 - n) + ")";
 }
 
-var value = new Uint8Array(1);
+var noise = (function () {
+	var value = new Uint8Array(1);
+	return function (x,y) {
+		value[0] = (200 * Math.floor(Math.sin(x / y + y + x) + Math.cos(y / x + x + y)) * x * y);
+		return value[0];
+	};
+})();
+
 
 self.onmessage = function (e) {
 	var result = [];
 	for (var i = 0; i < CHUNK_SIZE; i++) {
 		for (var j = 0; j < CHUNK_SIZE; j++) {
 			var x = (j + e.data.x) / 8, y = (i - e.data.y) / 8;
-			value[0] = (200 * Math.floor(Math.sin(x / y + y + x) + Math.cos(y / x + x + y)) * x * y);
-			var str = value[0].toString(10);
+			var value = noise(x,y);
+			var str = value.toString(10);
 			while (str.length < 3) {
 				str += "0";			
 			}
-			result.push("<span style=\"color:" + color(value[0]) + ";\"> " + str + "</span>");
+			result.push("<span style=\"color:" + color(value) + ";\"> " + str + "</span>");
 		}
 		result.push("<br>");
 	}
