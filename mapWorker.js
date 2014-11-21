8381e8b9b51c62e76e96614c2c1f1523cec2a760
zgike <theskullee@gmail.com>
2014-11-22 00:35:01 +1300

    meh

--- mapWorker.js
+++ mapWorker.js
@@ -6,9 +6,10 @@ self.onmessage = function (e) {
 	var chunk = new Uint8Array(CHUNK_SIZE * CHUNK_SIZE);
 	for (var i = 0; i < CHUNK_SIZE; i++) {
 		for (var j = 0; j < CHUNK_SIZE; j++) {
+			var x = (j + e.data.x) / 6, y = (i - e.data.y) / 6;
 			chunk[i * CHUNK_SIZE + j] =
-				200 * Math.floor(Math.sin((i -  e.data.y) / 2)
-				+ Math.sin((j + e.data.x) / 2));
+				200 * Math.floor(Math.sin(x / y + y + x)
+				+ Math.cos(y / x + x + y)) * x * y;
 		}
 	}
 	self.postMessage(chunk);
