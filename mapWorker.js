d17876c6065d435dce87e0dbaa6550dbbed8fdad
zgike <theskullee@gmail.com>
2014-11-21 22:11:02 +1300

    dumb stuff

--- mapWorker.js
+++ mapWorker.js
@@ -6,7 +6,7 @@ self.onmessage = function (e) {
 	var chunk = new Uint8Array(CHUNK_SIZE * CHUNK_SIZE);
 	for (var i = 0; i < CHUNK_SIZE; i++) {
 		for (var j = 0; j < CHUNK_SIZE; j++) {
-			chunk[i * CHUNK_SIZE + j] = Math.floor(Math.sin(i/10 + 0.577) * Math.sin(j/10 + 0.4424) * 10);
+			chunk[i * CHUNK_SIZE + j] = Math.floor(Math.sin(i/10) * Math.sin(j/10) * 10);
 		}
 	}
 	self.postMessage(chunk);
